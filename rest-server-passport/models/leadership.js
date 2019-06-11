d84df22b32dfffd3b80f5a0cf9caf2932400c98a
Prasham Anand <anandprasham@gmail.com>
2019-06-11 20:51:15 +0530

    Add files via upload

---
+++ rest-server-passport/models/leadership.js
@@ -0,0 +1,40 @@
+/**
+ * Created by marcos on 25/09/16.
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true,
+    },
+    abbr: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured:{
+        type:Boolean,
+        default:false
+    },
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
---
+++ rest-server-passport/models/promotions.js
@@ -0,0 +1,44 @@
+/**
+ * Created by marcos on 25/09/16.
+ */
+
+var mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+
+var Currency = mongoose.Types.Currency;
+var Schema = mongoose.Schema;
+
+var promoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        required: false,
+        default: ""
+    },
+    price: {
+        type: Currency,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured:{
+        type:Boolean,
+        default:false
+    },
+}, {
+    timestamps: true
+});
+
+var Promotions = mongoose.model('Promotion', promoSchema);
+
+module.exports = Promotions;
